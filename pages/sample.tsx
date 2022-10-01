b8ecc80466f16c6b6efc85d209eca56f960cc2cb
Travis Wagner <trvswgnr@gmail.com>
2022-09-30 20:34:12 -0400

    rm unused

--- pages/sample.tsx
+++
@@ -1,8 +0,0 @@
-// this page will be custom, not through plasmic (the route must not exist in plasmic)
-export default function SamplePage() {
-  return (
-    <div>
-      <h1>Sample Page</h1>
-    </div>
-  )
-}
