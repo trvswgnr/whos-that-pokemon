3181a2c2161e9c4c19f265679e702d64f43d997f
Travis Wagner <trvswgnr@gmail.com>
2022-10-05 23:31:04 -0400

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,10 @@ import { Container } from '~/components'
 
 export function Footer() {
   return (
-    <footer className="pt-10 pb-12 border-t bg-slate-900 border-slate-700">
+    <footer className="py-14 border-t bg-slate-900 border-slate-700">
       <Container className="text-center">
-        <div className="flex justify-center mb-5">
-        </div>
-        <p className="text-sm text-white">Made with 💜 by <a href="https://github.com/trvswgnr" className="underline hover:opacity-60">Travis Aaron Wagner</a></p>
+        <p className="text-sm text-white mb-3">Made with 💜 by <a href="https://github.com/trvswgnr" className="underline hover:opacity-70">Travis Aaron Wagner</a></p>
+        <p className="text-sm text-white">View project on <a href="https://github.com/trvswgnr/whos-that-pokemon" className="underline hover:opacity-70">GitHub</a></p>
       </Container>
     </footer>
   )
